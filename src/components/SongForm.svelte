0709b59333f7dd1a962f3905330c6bd4cb07717c
Sander Constantin <sander.constantin@student.kdg.be>
2023-06-10 14:58:29 +0200

    for/id

--- src/components/SongForm.svelte
+++ src/components/SongForm.svelte
@@ -52,25 +52,25 @@
         </div>
     {/if}
     <div class="mb-4">
-        <label class="block text-sm font-medium leading-6 text-gray-900">Artist</label>
+        <label class="block text-sm font-medium leading-6 text-gray-900" for="artist">Artist</label>
         <div class="relative mt-2 rounded-md shadow-sm">
-            <input type="text" required bind:value={artist}
+            <input type="text" required bind:value={artist} id="artist"
                    class="block w-full rounded-md border-0 py-1.5 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                    placeholder="Taylor Swift">
         </div>
     </div>
     <div class="mb-4">
-        <label class="block text-sm font-medium leading-6 text-gray-900">Title</label>
+        <label class="block text-sm font-medium leading-6 text-gray-900" for="title">Title</label>
         <div class="relative mt-2 rounded-md shadow-sm">
-            <input type="text" required bind:value={title}
+            <input type="text" required bind:value={title} id="title"
                    class="block w-full rounded-md border-0 py-1.5 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                    placeholder="Let it rain!">
         </div>
     </div>
     <div class="mb-4">
-        <label class="block text-sm font-medium leading-6 text-gray-900">File</label>
+        <label class="block text-sm font-medium leading-6 text-gray-900" for="file">File</label>
         <div class="relative mt-2 rounded-md shadow-sm">
-            <input accept=".mp3" type="file" bind:files
+            <input accept=".mp3" type="file" bind:files id="file"
                    class="block w-full rounded-md border-0 py-1.5 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
         </div>
     </div>
